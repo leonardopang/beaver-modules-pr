c9ed2a1fcb8c5bf630c5c30f7594b45b55079e63
Leonardo Pang <leonardopang12@gmail.com>
2022-09-01 18:54:30 -0300

    Update Feedback

--- modules/feedback/includes/frontend.php
+++ modules/feedback/includes/frontend.php
@@ -17,15 +17,17 @@ $feedback_query = new WP_Query($feedback_args);
       <div class="feedback-slider">
         <?php if ($feedback_query->have_posts()) : while ($feedback_query->have_posts()) : $feedback_query->the_post(); ?>
         <div class="feedback-slider--item">
-          <div class="image-container">
-            <?= file_get_contents(get_template_directory_uri() . '/assets/images/svg/icon-testimonal.svg'); ?>
-          </div>
-          <div class="text-container">
-            <?php the_content() ?>
-          </div>
-          <div class="person-container">
-            <h3><?php the_title() ?></h3>
-            <span>Driver</span>
+          <div class="feedback-slider-container--item">
+            <div class="image-container">
+              <?= file_get_contents(get_template_directory_uri() . '/assets/images/svg/icon-testimonal.svg'); ?>
+            </div>
+            <div class="text-container">
+              <?php the_content() ?>
+            </div>
+            <div class="person-container">
+              <h3><?php the_title() ?></h3>
+              <span>Driver</span>
+            </div>
           </div>
         </div>
         <?php
---
+++ modules/feedback/js/sliderFeedback.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import { tns } from '../../../node_modules/tiny-slider/src/tiny-slider'
+
+export default function () {
+  const slider = tns({
+    container: '.feedback-slider',
+    items: 1,
+    gutter: 16,
+    controls: true,
+    controlsText: ['<', '>'],
+    nav: true,
+    center: true,
+    mouseDrag: true,
+    touch: true,
+    navPosition: 'bottom',
+  })
+}
